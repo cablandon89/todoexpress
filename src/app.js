8c493d985767ca0dd2875221c9370776876a71a2
CesarB <cablandon89@gmail.com>
2021-11-07 09:42:50 -0500

    agregando cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const tasksRoutes = require('./routes/tasks.routes');
 const middlewareError = require('./middlewares/error.middleware');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(tasksRoutes);
 
 app.use(middlewareError);
